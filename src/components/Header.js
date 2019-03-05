9fcc59600048fbcccee14a2a07c3a2d2568619d2
Nathan <nathanlserrano@gmail.com>
2019-03-04 17:32:23 -0800

    Changed navbar and jumbotron colors and fonts

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const jumbotronStyle = {
-    paddingTop: '10px',
+    paddingTop: '40px',
     paddingBottom: '50px',
     marginBottom: '0px',
     boxShadow: "0px 0px 0px 0px rgba(0,0,0,0)"
@@ -15,14 +15,15 @@ function Header (props) {
     return (
     <div>
         <div className="navbar-fixed">
-            <nav>
+            <nav className="black">
                 <header style={headerStyles} className="center">SCORE: {props.score}/12</header>
             </nav>
         </div>
 
-        <div className="card-panel grey lighten-2 center" style={jumbotronStyle}>
-            <div className="container">
-                <h1>Clicky Game! (MGS Edition)</h1>
+        <div className="card-panel grey lighten-1 center" style={jumbotronStyle}>
+            <div className="jumbo">
+                <h1>CLICKY GAME!</h1>
+                <h3>(METAL GEAR SOLID EDITION)</h3>
                 <h5>Click on an image to earn points, but don't click on any more than once!</h5>
             </div>
         </div>
